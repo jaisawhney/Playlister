c0a943ab45a9509fb07c92068ce2fd95997c3ed3
Jai Sawhney <jai.sawhney1@gmail.com>
2021-10-20 16:49:36 -0700

    Display playlists

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+playlists = [
+    {'title': 'Cat Videos', 'description': 'Cats acting weird'},
+    {'title': '80\'s Music', 'description': 'Don\'t stop believing!'}
+]
 
 
-@app.route("/")
-def index():
-    return render_template("home.html", msg="Template test")
+@app.route('/')
+def playlists_index():
+    return render_template('playlists_index.html', playlists=playlists)
 
 
 if __name__ == "__main__":
