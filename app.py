7a7271a155f910b25f6a8cbe0712750afd9d23c1
Jai Sawhney <jai.sawhney1@gmail.com>
2021-10-20 17:16:04 -0700

    Users can create playlists

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 # DB Setup
@@ -9,10 +9,37 @@ playlists = db.playlists
 app = Flask(__name__)
 
 
+def video_url_creator(id_lst):
+    videos = []
+    for vid_id in id_lst:
+        video = "https://youtube.com/embed/" + vid_id
+        videos.append(video)
+    return videos
+
+
 # Routes
-@app.route('/')
+@app.route("/")
 def playlists_index():
-    return render_template('playlists_index.html', playlists=playlists.find())
+    return render_template("playlists_index.html", playlists=playlists.find())
+
+
+@app.route("/playlists/new")
+def playlists_new():
+    return render_template("playlists_new.html")
+
+
+@app.route("/playlists", methods=["POST"])
+def playlists_submit():
+    video_ids = request.form.get("video_ids").split()
+    videos = video_url_creator(video_ids)
+    playlist = {
+        "title": request.form.get("title"),
+        "description": request.form.get("description"),
+        "videos": videos,
+        "video_ids": video_ids
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for("playlists_index"))
 
 
 if __name__ == "__main__":
