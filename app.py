c81797ca4db07f9616c38f8f2049c5bbcef2ec28
Jai Sawhney <jai.sawhney1@gmail.com>
2021-10-31 00:53:40 -0700

    Prepare for Heroku

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
+import os
+
 # DB Setup
-client = MongoClient()
+host = os.environ.get("MONGODB_URI", "mongodb://localhost:27017/Playlister")
+client = MongoClient(host=host)
 db = client.Playlister
 playlists = db.playlists
 
@@ -80,4 +83,4 @@ def delete_playlist(playlist_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=os.environ.get("PORT", 5000))
